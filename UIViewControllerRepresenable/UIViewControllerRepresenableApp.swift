6152c7fb788501f304b618cd9bb0a081b5d20a8d
Christian Skorobogatow <christiansko@icloud.com>
2022-08-12 10:12:56 +0200

    Initial Commit

---
+++ UIViewControllerRepresenable/UIViewControllerRepresenableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresenableApp.swift
+//  UIViewControllerRepresenable
+//
+//  Created by Christian Skorobogatow on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresenableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
